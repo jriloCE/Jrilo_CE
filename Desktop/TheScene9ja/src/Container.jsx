7d421362a930b7091a84072d8e26876e5031ce09
Mr Blvck <complexjrilo.com>
2024-11-02 09:25:39 +0100

    Mr Blvck Staged

---
+++ Desktop/TheScene9ja/src/Container.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <div className='h-[300px] bg-[gold]'>
+
+    </div>
+  )
+}
+
+export default Container
---
+++ Desktop/TheScene9ja/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Container from './Container'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+      <Container/>
+  </StrictMode>,
+)
